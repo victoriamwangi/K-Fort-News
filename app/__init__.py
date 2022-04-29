e81a24581a48e38a67b6218d03a73b1a8e6d738e
victoria mwangi <victoriamwangi021@gmail.com>
2022-04-29 17:49:13 +0300

    feat: get news sources

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from .config import DevConfig
 
-app = Flask(__name__)
-
 app = Flask(__name__, instance_relative_config= True)
 
 app.config.from_object(DevConfig)
--- app/request.py
+++ app/request.py
@@ -1,4 +1,27 @@
 from app import app
+import urllib.request, json
+from .models import source
 
-api_key  = app.config['API_BASE_URL']
+Source = source.Source
+
+api_key  = app.config['API_ KEY']
+base_url = app.config['API_BASE_URL']
+
+
+def get_news(sources):
+    get_news_url = base_url.format(sources, api_key)
+    
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        print(get_news_url)
+        get_news_response = json.loads(get_news_data)
+        
+        news_results = None
+        
+        if get_news_response['results']:
+            news_results_list = get_news['results']
+            news_results = process_results(news_results_list)
+            
+return news_results
+            
 
